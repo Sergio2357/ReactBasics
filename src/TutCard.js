0ddaf2cdee3c7a1ca3dfd4c82d38ad3bd24d2772
Sergio Andres Cadena Zaia <scadena@MacBook-Air-de-Administrador.local>
2020-03-18 10:24:27 -0500

    tutorial card component integrated

---
+++ src/TutCard.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Card, ListGroup, ListGroupItem} from '../node_modules/react-bootstrap'
+import logo from './images/logo.svg'
+
+export default class TutCard extends React.Component {
+    constructor(props) {
+      super(props)
+  
+      this.state = {
+      }
+  
+    }
+    render() {
+      return (
+        <React.Fragment>
+            <Card style={{ width: '18rem', backgroundColor: 'aliceblue'}} text={'dark'}>
+            <Card.Img variant="top" src={logo} className="App-logo" alt="logo"/>
+            <Card.Body>
+                <Card.Title class='h2'>React Basics</Card.Title>
+                <Card.Text className="card-text" style={{size:'a3'}}>
+                This Tutorial is for the basic understanding of React
+                </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+                <ListGroupItem>Babel, Webpack, Routing</ListGroupItem>
+                <ListGroupItem>Create React App</ListGroupItem>
+                <ListGroupItem>Components</ListGroupItem>
+                <ListGroupItem>Basic Tutorial</ListGroupItem>
+            </ListGroup>
+            <Card.Body>
+                <Card.Link className="card-link" href="#">More Info</Card.Link>
+                <Card.Link classNAme="card-link" href="#">Git Repo</Card.Link>
+            </Card.Body>
+            </Card>
+        </React.Fragment>
+      )
+    }
+  }
