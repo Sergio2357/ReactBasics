eeffc98a9bd847151d3dd1b43f0c994499a8b730
Sergio Andres Cadena Zaia <scadena@MacBook-Air-de-Administrador.local>
2020-03-20 10:41:09 -0500

    listrender

--- src/Tutorials.js
+++ src/Tutorials.js
@@ -34,7 +34,7 @@ export default class Tutorials extends React.Component {
                             listTopics : ["Babel, Webpack, Routing", "Create React App", "Components", "Basic Tutorial"]}),
             JSON.stringify({title : "React Basics 2.0",
                             description : "This Tutorial is the continuation of a previous one (React Basics)",
-                            gitrepo : "https://github.com/Sergio2357/ReactBasics",
+                            gitrepo : "https://github.com/Sergio2357/ReactBasics/tree/listOfCards",
                             listTopics : ["State of Components", "Lifecycle", "Fetching Data"]})
         ]
       }
@@ -47,5 +47,3 @@ export default class Tutorials extends React.Component {
         )  
     }
   }
-
-
